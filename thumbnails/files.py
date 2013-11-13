395707e8b087426dc7b0f6b66a87a1095fb4eb08
steven <steven@ui.co.id>
2013-11-13 11:15:59 +0700

    Refactor usage of file class
    1. ImageField now has attr_class ThumbnailedImageFile
    2. New object Gallery. Called by ``foo.image.thumbnails``
    3. New object ThumbnailFile, returned by Gallery functions

--- thumbnails/files.py
+++ thumbnails/files.py
@@ -3,6 +3,7 @@ import os
 from django.db.models.fields.files import ImageFieldFile, FieldFile
 
 from .backends.metadata import DatabaseBackend
+from . import conf
 
 
 class SourceImage(ImageFieldFile):
@@ -12,38 +13,106 @@ class SourceImage(ImageFieldFile):
 
 
 class ThumbnailedImageFile(FieldFile):
+    _thumbnails = {}
 
     def __init__(self, *args, **kwargs):
         super(ThumbnailedImageFile, self).__init__(*args, **kwargs)
         self.metadata_backend = DatabaseBackend()
+        self.thumbnails = Gallery(metadata_backend=self.metadata_backend,
+                                  storage=self.storage,
+                                  source_image=self)
+
+    def save(self, name, content, save=True):
+        thumbnail = super(ThumbnailedImageFile, self).save(name, content, save)
+        self.metadata_backend.add_source(self.name)
+        return thumbnail
+
+
+class Gallery(object):
+
+    def __init__(self, *args, **kwargs):
+        self.metadata_backend = kwargs.pop('metadata_backend')
+        self.storage = kwargs.pop('storage')
+        self.source_image = kwargs.pop('source_image')
+        self._thumbnails = {}
+
+    def __getattr__(self, name):
+        if name in conf.SIZES.keys():
+            return self.get_thumbnail(name)
+        else:
+            return super(Gallery, self).__getattr__(name)
 
     def _get_thumbnail_name(self, size):
-        filename, extension = os.path.splitext(self.name)
+        filename, extension = os.path.splitext(self.source_image.name)
         return "%s_%s%s" % (filename, size, extension)
 
-    def get_thumbnail(self, size):
-        # 1. Get thumbnail from meta store
-        # 2. If it doesn't exist, create thumbnail and return it
-        thumbnail = self.metadata_backend.get_thumbnail(self.name, size)
+    def _get_thumbnails(self, size, metadata):
+        """
+        Cache thumbnail instances
+        """
+        thumbnail = self._thumbnails.get(size)
         if thumbnail is None:
-            return self.create_thumbnail(size)
+            thumbnail = ThumbnailedFile(metadata=metadata, storage=self.storage)
+            self._thumbnails[size] = thumbnail
         return thumbnail
+
+    def _purge_thumbnails_cache(self):
+        if hasattr(self, '_all_thumbnails'):
+            del self._all_thumbnails
+
+    def all(self):
+        # 1. Get all available sizes
+        # 2. Get or create all thumbnails
+        # 3. Return all thumbnails as list
+        if not hasattr(self, '_all_thumbnails'):
+            metadatas = self.metadata_backend.get_thumbnails(self.source_image.name)
+            thumbnails = [ThumbnailedFile(metadata=metadata, storage=self.storage) for metadata in metadatas]
+            self._all_thumbnails = thumbnails
+        return self._thumbnails
+
+    def get_thumbnail(self, size, create=True):
+        # 1. Get thumbnail metdata from meta store
+        # 2. If it doesn't exist, create thumbnail and return it
+        metadata = self.metadata_backend.get_thumbnail(self.source_image.name, size)
+        if metadata is None:
+            if create:
+                return self.create_thumbnail(size)
+            else:
+                return None
+        else:
+            return self._get_thumbnails(size, metadata)
 
     def create_thumbnail(self, size):
         # 1. Use Storage API to create a thumbnail (and get its filename)
         # 2. Call metadata_storage.add_thumbnail(self.name, size, filename)
         filename = self._get_thumbnail_name(size)
-        self.storage.save(filename, self.file)
-        return self.metadata_backend.add_thumbnail(self.name, size, filename)
-
+        self.storage.save(filename, self.source_image.file)
+        metadata = self.metadata_backend.add_thumbnail(self.source_image.name, size, filename)
+        thumbnail = ThumbnailedFile(metadata=metadata, storage=self.storage)
+        self._thumbnails[size] = thumbnail
+        self._purge_thumbnails_cache()
+        return thumbnail
 
     def delete_thumbnail(self, size):
         # 1. Use Storage API to delete thumbnail
         # 2. Call metadata_storage.remove_thumbnail(self.name, size)
         self.storage.delete(self._get_thumbnail_name(size))
-        self.metadata_backend.delete_thumbnail(self.name, size)
+        self.metadata_backend.delete_thumbnail(self.source_image.name, size)
+        self._purge_thumbnails_cache()
+        del(self._thumbnails[size])
 
-    def save(self, name, content, save=True):
-        thumbnail_file = super(ThumbnailedImageFile, self).save(name, content, save)
-        self.metadata_backend.add_source(self.name)
-        return thumbnail_file
+
+class ThumbnailedFile(object):
+    def __init__(self, *args, **kwargs):
+        self.metadata = kwargs.pop('metadata')
+        self.storage = kwargs.pop('storage')
+
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def name(self):
+        return self.metadata.name
+
+    def url(self):
+        return self.storage.url(self.name)
--- thumbnails/tests/fields.py
+++ thumbnails/tests/fields.py
@@ -3,8 +3,8 @@ import os
 from django.core.files import File
 from django.test import TestCase
 
+from thumbnails.files import ThumbnailedFile
 from .models import TestModel
-from thumbnails.backends.metadata import ImageMeta
 
 
 class ImageFieldTest(TestCase):
@@ -25,15 +25,38 @@ class ImageFieldTest(TestCase):
 
         # 1. Test for thumbnail creation
         self.assertFalse(os.path.isfile(os.path.join(avatar_folder, 'tests_small.png')))
-        thumb = self.instance.avatar.create_thumbnail(size='small')
+        thumb = self.instance.avatar.thumbnails.create_thumbnail(size='small')
         self.assertTrue(os.path.isfile(os.path.join(avatar_folder, 'tests_small.png')))
 
+        # Make sure the returned thumbnail is of thumbnail class, not metadata
+        self.assertTrue(isinstance(thumb, ThumbnailedFile))
+
         # 2. Test for getting thumbnail
-        img_meta = self.instance.avatar.get_thumbnail(size='small')
-        self.assertEqual(img_meta, ImageMeta(thumb.source.name ,thumb.name, thumb.size))
+        self.assertEqual(thumb, self.instance.avatar.thumbnails.get_thumbnail(size='small'))
 
         # 3. Test for thumbnail deletion
         self.assertTrue(os.path.isfile(os.path.join(avatar_folder, 'tests_small.png')))
-        self.instance.avatar.delete_thumbnail(size='small')
+        self.instance.avatar.thumbnails.delete_thumbnail(size='small')
         self.assertFalse(os.path.isfile(os.path.join(avatar_folder, 'tests_small.png')))
 
+    def test_thumbnail_field(self):
+        # Make sure gallery return the correct thumbnail
+        self.assertTrue(self.instance.avatar.thumbnails.small, ThumbnailedFile)
+        self.assertEqual(os.path.basename(self.instance.avatar.thumbnails.small.name),
+                         'tests_small.png')
+        self.assertTrue(self.instance.avatar.thumbnails.default, ThumbnailedFile)
+        self.assertEqual(os.path.basename(self.instance.avatar.thumbnails.default.name),
+                         'tests_default.png')
+        self.assertTrue(self.instance.avatar.thumbnails.large, ThumbnailedFile)
+        self.assertEqual(os.path.basename(self.instance.avatar.thumbnails.large.name),
+                         'tests_large.png')
+
+    def test_thumbnails_all(self):
+        self.assertEqual(self.instance.avatar.thumbnails.default, self.instance.avatar.thumbnails.all().get('default'))
+
+        large_thumb = self.instance.avatar.thumbnails.large
+        self.assertEqual(large_thumb, self.instance.avatar.thumbnails.all().get('large'))
+
+        # Cache for all thumbnails should be correctly updated on deletion
+        self.instance.avatar.thumbnails.delete_thumbnail('large')
+        self.assertEqual(self.instance.avatar.thumbnails.all().get('large'), None)
