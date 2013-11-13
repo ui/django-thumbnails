abba2de917555a9e59ff0f0eb40bede917f97a52
steven <steven@ui.co.id>
2013-11-13 09:02:45 +0700

    Fix #1 renamed backend -> metadata_backend in ThumbnailedImageField

--- thumbnails/files.py
+++ thumbnails/files.py
@@ -15,7 +15,7 @@ class ThumbnailedImageFile(FieldFile):
 
     def __init__(self, *args, **kwargs):
         super(ThumbnailedImageFile, self).__init__(*args, **kwargs)
-        self.backend = DatabaseBackend()
+        self.metadata_backend = DatabaseBackend()
 
     def _get_thumbnail_name(self, size):
         filename, extension = os.path.splitext(self.name)
@@ -24,7 +24,7 @@ class ThumbnailedImageFile(FieldFile):
     def get_thumbnail(self, size):
         # 1. Get thumbnail from meta store
         # 2. If it doesn't exist, create thumbnail and return it
-        thumbnail = self.backend.get_thumbnail(self.name, size)
+        thumbnail = self.metadata_backend.get_thumbnail(self.name, size)
         if thumbnail is None:
             return self.create_thumbnail(size)
         return thumbnail
@@ -34,16 +34,16 @@ class ThumbnailedImageFile(FieldFile):
         # 2. Call metadata_storage.add_thumbnail(self.name, size, filename)
         filename = self._get_thumbnail_name(size)
         self.storage.save(filename, self.file)
-        return self.backend.add_thumbnail(self.name, size, filename)
+        return self.metadata_backend.add_thumbnail(self.name, size, filename)
 
 
     def delete_thumbnail(self, size):
         # 1. Use Storage API to delete thumbnail
         # 2. Call metadata_storage.remove_thumbnail(self.name, size)
         self.storage.delete(self._get_thumbnail_name(size))
-        self.backend.delete_thumbnail(self.name, size)
+        self.metadata_backend.delete_thumbnail(self.name, size)
 
     def save(self, name, content, save=True):
         thumbnail_file = super(ThumbnailedImageFile, self).save(name, content, save)
-        self.backend.add_source(self.name)
+        self.metadata_backend.add_source(self.name)
         return thumbnail_file
