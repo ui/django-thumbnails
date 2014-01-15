883e3652e95dd5aee625cd7ca0d61086ec65cc44
steven <steven@ui.co.id>
2014-01-15 15:38:48 +0700

    Added new settings POST_PROCESSORS

--- thumbnails/post_processors.py
+++ thumbnails/post_processors.py
@@ -19,14 +19,17 @@ def process(thumbnail_file, **kwargs):
     Post processors are functions that receive file objects,
     performs necessary operations and the results as file objects.
     """
+    from . import conf
     temp_dir = get_or_create_temporary_folder()
-    thumbnail_filename = os.path.join(temp_dir, uuid.uuid4().hex)
+    thumbnail_filename = os.path.join(temp_dir, "%s.png" % uuid.uuid4().hex)
 
     f = open(thumbnail_filename, 'wb')
     f.write(thumbnail_file.read())
     f.close()
 
-    optimize_image(thumbnail_filename)
+    for post_processor in conf.POST_PROCESSORS['processors']:
+        post_processor(thumbnail_filename)
+
     optimized_file = File(open(thumbnail_filename, 'rb'))
     # _get_size() is needed to prevent Django < 1.5 from throwing an AttributeError.
     # This is fixed in https://github.com/django/django/commit/5c954136eaef3d98d532368deec4c19cf892f664
@@ -42,25 +45,16 @@ def optimize_image(thumbnail_path):
     Method to optimize image using tools that are available.
     Logic is taken from image-diet https://github.com/samastur/image-diet/blob/master/image_diet/diet.py
     """
-
+    from . import conf
     # Detect filetype
     filetype = imghdr.what(thumbnail_path)
 
     # Construct command to optimize image based on filetype
-    commands = []
-    if filetype == "jpeg":
-        commands.append("jpegoptim -f --strip-all '%(file)s'")
-    elif filetype == "png":
-        commands.append("optipng -force -o7 '%(file)s'")
-        #commands.append(
-            #("pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB "
-             #"-rem time '%(file)s' '%(file)s.diet' "
-             #"&& mv '%(file)s.diet' '%(file)s'")
-        #)
+    command = conf.POST_PROCESSORS.get("%s_command" % filetype)
 
     # Run Command
-    if commands:
-        command = " && ".join(commands) % {'file': thumbnail_path}
+    if command:
+        command = command % {'filename': thumbnail_path}
         try:
             status_code = call(command, shell=True, stdout=PIPE)
         except:
--- thumbnails/utils.py
+++ thumbnails/utils.py
@@ -6,6 +6,8 @@ def import_attribute(name):
     Return an attribute from a dotted path name (e.g. "path.to.func").
     Copied from nvie's rq https://github.com/nvie/rq/blob/master/rq/utils.py
     """
+    if hasattr(name, '__call__'):
+        return name
     module_name, attribute = name.rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, attribute)
