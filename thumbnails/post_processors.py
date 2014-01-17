295f948107b24668e27fc74b49931d8f83e85cc0
Steven <SeiryuZ@hotmail.com>
2014-01-17 09:28:35 +0700

    Moved file operations to optimize_image method

--- thumbnails/post_processors.py
+++ thumbnails/post_processors.py
@@ -20,6 +20,20 @@ def process(thumbnail_file, **kwargs):
     performs necessary operations and the results as file objects.
     """
     from . import conf
+
+    for post_processor in conf.POST_PROCESSORS['processors']:
+        post_processor(thumbnail_file)
+
+    return thumbnail_file
+
+
+def optimize_image(thumbnail_file):
+    """
+    Method to optimize image using tools that are available.
+    Logic is taken from image-diet https://github.com/samastur/image-diet/blob/master/image_diet/diet.py
+    """
+    from . import conf
+
     temp_dir = get_or_create_temporary_folder()
     thumbnail_filename = os.path.join(temp_dir, "%s.png" % uuid.uuid4().hex)
 
@@ -27,8 +41,22 @@ def process(thumbnail_file, **kwargs):
     f.write(thumbnail_file.read())
     f.close()
 
-    for post_processor in conf.POST_PROCESSORS['processors']:
-        post_processor(thumbnail_filename)
+    # Detect filetype
+    filetype = imghdr.what(thumbnail_filename)
+
+    # Construct command to optimize image based on filetype
+    command = conf.POST_PROCESSORS.get("%s_command" % filetype)
+
+    # Run Command
+    if command:
+        command = command % {'filename': thumbnail_filename}
+        try:
+            status_code = call(command, shell=True, stdout=PIPE)
+        except:
+            raise ValueError('Cannot optimize image with parameters %s', command)
+        if status_code != 0:
+            # Failed.
+            raise ValueError('Cannot optimize image. Missing utilities')
 
     optimized_file = File(open(thumbnail_filename, 'rb'))
     # _get_size() is needed to prevent Django < 1.5 from throwing an AttributeError.
@@ -37,29 +65,5 @@ def process(thumbnail_file, **kwargs):
     optimized_file._get_size()
 
     os.remove(thumbnail_filename)
+
     return optimized_file
-
-
-def optimize_image(thumbnail_path):
-    """
-    Method to optimize image using tools that are available.
-    Logic is taken from image-diet https://github.com/samastur/image-diet/blob/master/image_diet/diet.py
-    """
-    from . import conf
-    # Detect filetype
-    filetype = imghdr.what(thumbnail_path)
-
-    # Construct command to optimize image based on filetype
-    command = conf.POST_PROCESSORS.get("%s_command" % filetype)
-
-    # Run Command
-    if command:
-        command = command % {'filename': thumbnail_path}
-        try:
-            status_code = call(command, shell=True, stdout=PIPE)
-        except:
-            raise ValueError('Cannot optimize image with parameters %s', command)
-        if status_code != 0:
-            # Failed.
-            raise ValueError('Cannot optimize image. Missing utilities')
-    return thumbnail_path
