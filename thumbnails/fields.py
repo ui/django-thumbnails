d9a2d361ca07d2c809f34c6acc917d5a5509053b
Selwin Ong <selwin.ong@gmail.com>
2014-05-12 08:44:41 +0700

    Fixed typo in ImageField's super call.

--- thumbnails/fields.py
+++ thumbnails/fields.py
@@ -17,7 +17,7 @@ class ImageField(DjangoImageField):
             raise ValueError('Please define storage backend in settings.py instead on the field itself')
         kwargs['storage'] = storage.get_backend()
         self.metadata_backend = metadata.get_backend()
-        super(ImageField, self).__init__(self, *args, **kwargs)
+        super(ImageField, self).__init__(*args, **kwargs)
 
     def __unicode__(self):
         return self.attname
