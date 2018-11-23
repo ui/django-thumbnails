ff57998038da8fdb60505266a9e0415c736621c0
Selwin Ong <selwin.ong@gmail.com>
2018-11-23 18:58:42 +0700

    Updated migrations to work with Django 2.X

--- thumbnails/migrations/0001_initial.py
+++ thumbnails/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('size', models.CharField(max_length=64)),
                 ('name', models.CharField(unique=True, max_length=255)),
-                ('source', models.ForeignKey(related_name=b'thumbnails', to='thumbnails.Source')),
+                ('source', models.ForeignKey(on_delete=models.deletion.CASCADE, related_name='thumbnails', to='thumbnails.Source')),
             ],
             options={
             },
