1a6fc239acb40b76ffb7adf3f94c6c0b9a152d95
yosephbernandus <yosephbernandus@gmail.com>
2020-10-28 10:28:26 +0700

    Automatically convert image from given parameter extension

--- thumbnails/tests/models.py
+++ thumbnails/tests/models.py
@@ -6,3 +6,4 @@ from thumbnails.fields import ImageField
 class TestModel(models.Model):
     avatar = ImageField(upload_to='avatars', resize_source_to='source')
     profile_picture = ImageField(upload_to='avatars', blank=True, null=True, resize_source_to='source')
+    card_identity_picture = ImageField(upload_to='identity_card', blank=True, null=True, resize_source_to='source', convert_to="webp")
