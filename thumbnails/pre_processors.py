94c6ce191611e96d1d5d5ef2735e2f6bfd5a2cc7
marsha <angelamdm97@gmail.com>
2021-05-11 16:38:08 +0700

    basic watermarking

--- thumbnails/pre_processors.py
+++ thumbnails/pre_processors.py
@@ -3,8 +3,8 @@ from da_vinci import images
 from .utils import write_to_content_file
 
 
-def attach_watermark(image, watermark_path):
-    raw_image = images.from_file(image)
+def attach_watermark(image_path, watermark_path):
+    raw_image = images.from_file(image_path)
     watermark_image = images.from_file(watermark_path)
     pil_image = raw_image.get_pil_image()
     watermark_pil_image = watermark_image.get_pil_image()
@@ -12,11 +12,8 @@ def attach_watermark(image, watermark_path):
         # TODO: parse watermark dynamically based on ratio
         raise ValueError("Watermark image should have the same dimension as image")
 
-    if watermark_image.format != "PNG":
-        raise ValueError("Watermark must be PNG")
-
-    if watermark_pil_image.mode != "RGBA":
-        watermark_pil_image = watermark_pil_image.convert(mode="RGBA")
+    if watermark_image.format != "PNG" or watermark_pil_image.mode != "RGBA":
+        raise ValueError("Watermark must be PNG and containts alpha")
 
     pil_image.paste(watermark_pil_image, (0, 0), watermark_pil_image)
     raw_image.set_pil_image(pil_image)
---
+++ thumbnails/tests/test_pre_processors.py
@@ -0,0 +1,49 @@
+import os
+
+from django.test import TestCase
+from django.core.files import File
+from PIL import Image
+
+from thumbnails.pre_processors import attach_watermark
+from thumbnails.utils import write_to_content_file
+
+from .storage import TemporaryStorage
+
+
+class PreProcessorsTest(TestCase):
+
+    def test_attach_watermark(self):
+        image_path = 'thumbnails/tests/tests.png'
+        watermark_path = os.path.join(TemporaryStorage().temporary_location, "test_watermark.png") 
+        original_image_path = os.path.join(TemporaryStorage().temporary_location, "test_bg.png") 
+        watermark_image = Image.open(image_path)
+
+        with open('thumbnails/tests/tests.png', 'rb') as image_file:
+            original_file = File(image_file)
+
+            # not rgba
+            converted_watermark = watermark_image.convert("RGB")
+            converted_watermark.save(watermark_path)
+
+            self.assertRaises(ValueError, attach_watermark, original_file, watermark_path)
+
+        converted_watermark = watermark_image.convert("RGBA")
+        converted_watermark.save(watermark_path)
+
+        # different size
+        original_image = Image.open(image_path)
+        converted_image = original_image.resize((300, 300))
+        converted_image.save(original_image_path)
+
+        converted_watermark = converted_watermark.resize((200, 200))
+        converted_watermark.save(watermark_path)
+
+        with open(original_image_path, 'rb') as image_file:
+            self.assertRaises(ValueError, attach_watermark, image_file, watermark_path)
+
+        converted_image = original_image.resize((200, 200))
+        converted_image.save(original_image_path)
+
+        # no error raised
+        with open(original_image_path, 'rb') as image_file:
+            attach_watermark(image_file, watermark_path)
