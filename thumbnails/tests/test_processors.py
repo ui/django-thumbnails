26a62af2a417a2e8ec342b59cc0e6822a87f9393
marsha <angelamdm97@gmail.com>
2021-05-17 16:57:57 +0700

    simplify test

--- thumbnails/tests/test_processors.py
+++ thumbnails/tests/test_processors.py
@@ -15,14 +15,9 @@ class ProcessorsTest(TestCase):
     def test_add_watermark(self):
         image_path = 'thumbnails/tests/tests.png'
         watermark_path = os.path.join(TemporaryStorage().temporary_location, "test_watermark.png") 
-        original_image_path = os.path.join(TemporaryStorage().temporary_location, "test_bg.png") 
 
         # different size
         original_image_file = images.from_file(image_path)
-        original_image = original_image_file.get_pil_image()
-        converted_image = original_image.resize((300, 300))
-        converted_image.save(original_image_path)
-        original_image_file.set_pil_image(converted_image)
 
         watermark_image = Image.open(image_path)
         converted_watermark = watermark_image.resize((200, 200))
@@ -34,10 +29,6 @@ class ProcessorsTest(TestCase):
             "path": watermark_path
         }
         self.assertRaises(ValueError, add_watermark, original_image_file, **kwargs)
-
-        converted_image = original_image.resize((200, 200))
-        converted_image.save(original_image_path)
-        original_image_file.set_pil_image(converted_image)
 
         # not rgba
         converted_watermark = converted_watermark.convert("RGB")
