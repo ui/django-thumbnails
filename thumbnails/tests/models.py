165cd0c603f99860091b81f50ef57a88134b60e5
marsha97 <angelamdm97@gmail.com>
2023-01-17 10:32:16 +0700

    rename metadata to metadata_backend

--- thumbnails/tests/models.py
+++ thumbnails/tests/models.py
@@ -10,7 +10,7 @@ class TestModel(models.Model):
     card_identity_picture = ImageField(upload_to='identity_card', blank=True, null=True,
                                        resize_source_to='source_with_format',
                                        storage=storage.TemporaryStorage2(),
-                                       metadata=metadata.CustomRedisBackend())
+                                       metadata_backend=metadata.CustomRedisBackend())
 
 
 class TestPregeneratedSizesModel(models.Model):
